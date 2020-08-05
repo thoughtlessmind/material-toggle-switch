bed16ba8bc70acbd97f6a7673797eabd73bfed20
Rajiv <51883613+thoughtlessmind@users.noreply.github.com>
2020-08-05 23:22:22 +0530

    toggle btn

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React, { useEffect, useState } from "react";
 import ToggleSwitch from "./ToggleSwitch";
-import axios from "axios";
 import "./styles.css";
 
 export default function App() {
   const [loading, setLoading] = useState(false);
   const [toggleState, setToggleState] = useState(true);
 
-  const clickHandler = () => {
-    setLoading(!loading);
-    setTimeout(() => {
-      setLoading(!loading);
-    }, 2000);
-  };
+
 
   const a = () => {
     setToggleState(!toggleState);
@@ -27,21 +21,14 @@ export default function App() {
     }, 2000);
   };
 
-  const getCall = () => {
-    setLoading(true);
-    axios.get("https://jsonplaceholder.typicode.com/users").then(res => {
-      setLoading(false);
-      console.log(res.data);
-    });
-  };
+
 
   useEffect(() => console.log("loading state", loading), [loading]);
 
   return (
     <div className="App" style={{ height: "100vh" }}>
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-      <div style={{margin: '20px 40%'}}>
+      <h1 className="heading">Toggle switch with loading animation inside</h1>
+      <div className='container'>
         <ToggleSwitch loading={loading} onChange={fn1} checked={toggleState} />
         <div style={{ textAlign: "left" }}>
           <h4>Loading state: {`${loading}`}</h4>
